7f05c1ffa666f48dd6ef69c2de9fbcd00a93864f
Bruno <bruno.groth@hotmail.com>
2024-02-03 04:36:43 -0300

    Feat: Setup project

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WebManager</title>
+</head>
+
+<body>
+  <?php phpinfo() ?>
+</body>
+
+</html>
